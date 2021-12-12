df94b0a143ea4557a4a4ec18105d736956010621
Viktor <vityaschel@utidteam.com>
2021-12-12 13:58:31 +0400

    OpenGraph Protocol image tag added

---
+++ out/_next/static/ijWjdNL-xZ0ponDUiGpbn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5b20585dc8229bd7.css","static/chunks/pages/index-1822b57531f54c8a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
